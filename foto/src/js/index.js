abb553482aac12f4491b56d5d490704208cb05b7
Brandon Henry <bhenry@campaignmonitor.com>
2020-03-23 03:01:33 -0400

    add router

--- foto/src/js/index.js
+++ foto/src/js/index.js
@@ -3,10 +3,49 @@
 const e = React.createElement;
 
 class App extends React.Component {
+    readURL() {
+        var path = location.href && location.href.split("#")[1];
+        var parts = path && path.split("/");
+        parts && !parts[parts.length - 1] && parts.pop();
+        parts && !parts[0] && parts.shift();
+        return parts;
+    }
+    match(path, matcher) {
+        var path_params = {}
+        for (var i = 0; i < matcher.length; i++) {
+            if (matcher[i].startsWith("?")){
+                path_params[matcher[i].substring(1)] = path[i];
+            } else if (matcher[i] !== path[i]) {
+                return false;
+            }
+        }
+        console.log(path_params);
+        path_params["codeWord"] = "wtf";
+        return path_params;
+    }
+    constructor(props) {
+        super(props);
+        this.state = {
+            path: this.readURL()
+        };
+    }
+    componentDidMount(props) {
+        window.onhashchange = () => {
+            this.setState({
+                path: this.readURL()
+            });
+       };
+    }
     render() {
-        return (
-            <div>hi</div>
-        )
+        if (this.match(this.readURL(), ["home"])) {
+            return (
+                <div>home</div>
+            )
+        } else {
+            return (
+                <div>Not found</div>
+            )
+        }
     }
 }
 
